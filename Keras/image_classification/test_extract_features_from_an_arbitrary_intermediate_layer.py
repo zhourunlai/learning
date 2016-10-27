da9a2b1b0f79380ad4fdeb626ed5f0a6c244569b
zhourunlai <u201316277@hust.edu.cn>
2016-10-27 15:21:46 +0800

    add extract features

---
+++ Keras/image_classification/test_extract_features_from_an_arbitrary_intermediate_layer.py
@@ -0,0 +1,17 @@
+from vgg19 import VGG19
+from keras.preprocessing import image
+from imagenet_utils import preprocess_input
+from keras.models import Model
+
+base_model = VGG19(weights='imagenet')
+model = Model(input=base_model.input, output=base_model.get_layer('block4_pool').output)
+
+img_path = 'test.jpeg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+block4_pool_features = model.predict(x)
+
+print block4_pool_features
---
+++ Keras/image_classification/test_extract_features_from_images.py
@@ -0,0 +1,15 @@
+from vgg16 import VGG16
+from keras.preprocessing import image
+from imagenet_utils import preprocess_input
+
+model = VGG16(weights='imagenet', include_top=False)
+
+img_path = 'test.jpeg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+features = model.predict(x)
+
+print features
